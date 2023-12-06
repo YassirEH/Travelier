10d7e453d342f4ff38a8a10b95fc9aeb6b12b9f2
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-06 16:21:34 +0100

    added and used icon, title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
+import Travelier from "../images/Travelier.png";
 
 const Navbar = () => {
 return (
-    <div>
+    <div className="navbar">
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Services</li>
-                <li>Tours</li>
+            <img className="logo" src={Travelier} alt="logo" />
+            <ul className="services">
+                
             </ul>
         </nav>
     </div>
