8640c4f0f1b0d1b0a5e1495fff687bc205ced97a
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-06 15:58:06 +0100

    removed readme

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+return (
+    <div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Tours</li>
+            </ul>
+        </nav>
+    </div>
+);
+}
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,78 @@
+
+const socialMediaLinks = {
+    facebook: {
+        id: 1,
+        href: "https://www.facebook.com/travelier",
+        text: "Facebook",
+    },
+    twitter: {
+        id: 2,
+        href: "https://www.twitter.com/travelier",
+        text: "Twitter",
+    },
+    instagram: {
+        id: 3,
+        href: "https://www.instagram.com/travelier",
+        text: "Instagram",
+    },
+};
+
+const pageLinks = {
+    home: {
+        id: 1,
+        href: "#home",
+        text: "Home",
+    },
+    about: {
+        id: 2,
+        href: "#about",
+        text: "About",
+    },
+    contact: {
+        id: 3,
+        href: "#contact",
+        text: "Contact",
+    },
+};
+
+const services = [
+    {
+        // END: abpxx6d04wxr
+        id: 1,
+        title: "Flight Booking",
+        description: "Book your flights with ease.",
+    },
+    {
+        id: 2,
+        title: "Hotel Reservations",
+        description: "Find the perfect hotel for your stay.",
+    },
+    {
+        id: 3,
+        title: "Car Rentals",
+        description: "Rent a car for your travel needs.",
+    },
+];
+
+const tours = [
+    {
+        id: 1,
+        title: "City Tour",
+        description: "Explore the city's landmarks and attractions.",
+        duration: "3 hours",
+    },
+    {
+        id: 2,
+        title: "Beach Getaway",
+        description: "Relax and unwind on the beautiful beaches.",
+        duration: "2 days",
+    },
+    {
+        id: 3,
+        title: "Adventure Trek",
+        description: "Embark on an exciting trekking adventure.",
+        duration: "5 days",
+    },
+];
+
+export { socialMediaLinks, pageLinks, services, tours };
