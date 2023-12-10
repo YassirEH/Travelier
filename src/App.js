4762e5c3f50996d05466b8e8e29a06c2a03e0c5b
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-10 18:45:57 +0100

    added the home page as well as fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <React.Fragment>
+        <Navbar />
+        <Home />
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/Home.css";
+const Home = () => {
+    return (
+        <div className="home">
+            <section id="home"></section>
+            <div className="home-container">
+                <h1>Continue Exploring</h1>
+                <p>Travelier is your passport to the world's wonders. It helps you plan trips, discover hidden gems, and craft unique itineraries.
+                    <br></br>Experience stunning landscapes, vibrant cultures, and diverse cuisines with ease.
+                    <br></br>Travelier is your trusted companion for unforgettable journeys.
+                    <br></br> Explore the world's splendor with Travelier.</p>
+                <a href="#tours" className="explore_button">EXPLORE TOURS</a>
+            </div>
+        </div>
+    );
+};
+export default Home;
+
+<p>  </p>
